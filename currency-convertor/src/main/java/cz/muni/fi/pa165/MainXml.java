4762b914ef7c4c4b9109ccd56361cac9a02fc94f
Ondrej Holub <ondrejholub202@gmail.com>
2021-03-16 15:44:22 +0100

    Task 4 finished

---
+++ currency-convertor/src/main/java/cz/muni/fi/pa165/MainXml.java
@@ -0,0 +1,22 @@
+package cz.muni.fi.pa165;
+
+import cz.muni.fi.pa165.currency.CurrencyConvertor;
+import cz.muni.fi.pa165.currency.ExchangeRateTable;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+import java.math.BigDecimal;
+import java.util.Currency;
+
+public class MainXml {
+
+
+    public static void springXmlContext(){
+        ApplicationContext context  = new ClassPathXmlApplicationContext("applicationContext.xml");
+        ExchangeRateTable exchangeRateTable = context.getBean("exchangeRateTable", ExchangeRateTable.class);
+        CurrencyConvertor currencyConvertor = context.getBean("currencyConvertor", CurrencyConvertor.class);
+
+        BigDecimal rate = currencyConvertor.convert(Currency.getInstance("EUR"), Currency.getInstance("CZK"), new BigDecimal("1"));
+        System.out.println(rate);
+    }
+}
---
+++ currency-convertor/src/test/java/cz/muni/fi/pa165/MainXmlTest.java
@@ -0,0 +1,16 @@
+package cz.muni.fi.pa165;
+
+import org.junit.Test;
+
+
+
+public class MainXmlTest {
+
+    @Test
+    public void testConvert()  {
+
+        MainXml.springXmlContext();
+    }
+
+
+}
